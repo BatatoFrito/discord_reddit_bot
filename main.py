a677f154cc2d53008c025d3a8e1870fa0d6b9d64
Batato <batatofritogames@gmail.com>
2024-10-06 14:33:43 -0300

    discord bot and loop added

--- main.py
+++ main.py
@@ -1,19 +1,58 @@
 '''
 
-This bot grabs posts from subreddits and automatically sends them on Discord
+This bot gets posts from subreddits and automatically sends them on Discord
 
 '''
 
+import discord
 import praw
+from discord.ext import commands, tasks
 from dotenv import dotenv_values
 
 # Reddit app values
 config = dotenv_values('.env')
 
-USERNAME = config['USERNAME']
-PASSWORD = config['PASSWORD']
-CLIENT_ID = config['CLIENT_ID']
-CLIENT_SECRET = config['CLIENT_SECRET']
-USER_AGENT = config['USER_AGENT']
+REDDIT_USERNAME = config['REDDIT_USERNAME']
+REDDIT_PASSWORD = config['REDDIT_PASSWORD']
+REDDIT_CLIENT_ID = config['REDDIT_CLIENT_ID']
+REDDIT_CLIENT_SECRET = config['REDDIT_CLIENT_SECRET']
+REDDIT_USER_AGENT = config['REDDIT_USER_AGENT']
 
-reddit_instance = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, password=PASSWORD, user_agent=USER_AGENT, username=USERNAME)
+reddit_instance = praw.Reddit(client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET, password=REDDIT_PASSWORD, 
+                              user_agent=REDDIT_USER_AGENT, username=REDDIT_USERNAME)
+
+# Discord bot values
+DISCORD_TOKEN = config['DISCORD_TOKEN']
+intents = discord.Intents.default()
+
+client = commands.Bot(command_prefix='!', intents=intents)
+
+# Subreddits that the bot will monitor
+sub_list = ['cats']
+subreddits = {}
+submissions = {}
+last_submissions = []
+
+# Creates the subs dict
+for sub in sub_list:
+    subreddits[f'{sub}'] = reddit_instance.subreddit(f'{sub}')
+
+@client.event
+async def on_ready():
+    await sub_update.start()
+
+# Newest post loop
+@tasks.loop(seconds=5)
+async def sub_update():
+    channel = client.get_channel(1027351888053682249)
+    # Sends newest posts
+    for sub in sub_list:
+        if f'{sub}' in submissions:
+            if submissions[f'{sub}'] == next(subreddits[f'{sub}'].new(limit=1)):
+                continue
+        
+        submissions[f'{sub}'] = next(subreddits[f'{sub}'].new(limit=1))
+        await channel.send(submissions[f'{sub}'].title)
+
+# Runs Discord Bot
+client.run(DISCORD_TOKEN)
