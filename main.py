8d04595a4ecb7e646824ea5f7dda5d5f56a5c8ce
Batato <batatofritogames@gmail.com>
2024-10-09 04:25:37 -0300

    loop working and remove from channel added

--- main.py
+++ main.py
@@ -56,15 +56,8 @@ intents = discord.Intents.default()
 
 client = commands.Bot(command_prefix='!', intents=intents)
 
-# Subreddits that the bot will monitor
-sub_list = ['cats', 'AceAttorney']
-subreddits = {}
-submissions = {}
-last_submissions = []
-
-# Creates the subs dict
-for sub in sub_list:
-    subreddits[f'{sub}'] = reddit_instance.subreddit(f'{sub}')
+# Latest submissions list
+latest_submissions = []
 
 @client.event
 async def on_ready():
@@ -73,17 +66,43 @@ async def on_ready():
     await sub_update.start()
 
 # Newest post loop
-@tasks.loop(seconds=5)
+@tasks.loop(seconds=10)
 async def sub_update():
-    channel = client.get_channel(1027351888053682249)
+    # Subreddits that the bot will monitor
+    sub_list = []
+    sub_db = session.query(Subreddit).all()
+
+    for sub in sub_db:
+        # Checks if subreddit exists or is banned
+        try:
+            reddit_instance.subreddits.search_by_name(sub.name, exact=True)
+        except(NotFound, Redirect):
+            continue
+        sub_list.append(sub)
+
+    subreddits = {}
+    submissions = {}
+
+    # Creates the subs dict
+    for sub in sub_list:
+        if sub.name in subreddits:
+            continue
+        else:
+            subreddits[f'{sub.name}'] = reddit_instance.subreddit(f'{sub.name}')
+
     # Sends newest posts
     for sub in sub_list:
-        if f'{sub}' in submissions:
-            if submissions[f'{sub}'] == next(subreddits[f'{sub}'].new(limit=1)):
-                continue
-        
-        submissions[f'{sub}'] = next(subreddits[f'{sub}'].new(limit=1))
-        await channel.send(submissions[f'{sub}'].title)
+        channel = client.get_channel(sub.channel_id)
+        submissions[f'{sub.name}'] = next(subreddits[f'{sub.name}'].new(limit=1))
+        content = submissions[f'{sub.name}']
+        content_check = (content.id, channel.id)
+
+        # Checks if has already been sent
+        if content_check in latest_submissions:
+            continue
+
+        await channel.send(content.title)
+        latest_submissions.append(content_check)
 
 # Set channel command
 @client.tree.command(name='set_sub_to_channel', 
@@ -118,6 +137,30 @@ async def sub_to_channel(interaction: discord.Interaction, subs: str):
         await interaction.response.send_message(f'Set {successes} new subreddit(s) to this channel!\nExceptions: {exceptions}', ephemeral=True)
     else:
         await interaction.response.send_message(f'Set {successes} new subreddit(s) to this channel!', ephemeral=True)
+
+# Remove from channel command
+@client.tree.command(name='remove_sub_from_channel', 
+                     description='Removes subs that were set in a channel, multiple subreddits can be removed separating by space.')
+async def remove_from_channel(interaction: discord.Interaction, subs: str):
+    subs_list = subs.split()
+    channel = interaction.channel
+    successes = 0
+    exceptions = []
+
+    for sub in subs_list:
+        sub_query = session.query(Subreddit).filter(and_(Subreddit.name == sub, Subreddit.channel_id == channel.id)).first()
+
+        if sub_query:
+            session.delete(sub_query)
+            session.commit()
+            successes += 1
+        else:
+            exceptions.append(sub)
+
+    if exceptions:
+        await interaction.response.send_message(f'Removed {successes} subreddit(s) from this channel!\nExceptions: {exceptions}', ephemeral=True)
+    else:
+        await interaction.response.send_message(f'Removed {successes} subreddit(s) from this channel!', ephemeral=True)
 
 # Runs Discord Bot
 if __name__ == '__main__':
