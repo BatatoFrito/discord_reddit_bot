b6bf3f36814c7aa0ba3417ea3f097d72c1631afe
Batato <batatofritogames@gmail.com>
2024-10-09 02:52:14 -0300

    database and sub to channel command added

--- main.py
+++ main.py
@@ -6,8 +6,36 @@ This bot gets posts from subreddits and automatically sends them on Discord
 
 import discord
 import praw
+from sqlalchemy import create_engine, Column, String, Integer, and_, exists
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from pathlib import Path
 from discord.ext import commands, tasks
 from dotenv import dotenv_values
+
+# Database settings
+Base = declarative_base()
+
+class Subreddit(Base):
+    __tablename__ = 'subreddits'
+
+    _id = Column('id', Integer, primary_key=True)
+    guild_id = Column('guild_id', Integer)
+    channel_id = Column('channel_id', Integer)
+    name = Column('name', String)
+
+    def __init__(self, guild_id, channel_id, name):
+        self.guild_id = guild_id
+        self.channel_id = channel_id
+        self.name = name
+
+engine = create_engine('sqlite:///subreddits.db')
+Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Paths
+FILE_PATH = Path(__file__).absolute().parent
 
 # Reddit app values
 config = dotenv_values('.env')
@@ -19,7 +47,7 @@ REDDIT_CLIENT_SECRET = config['REDDIT_CLIENT_SECRET']
 REDDIT_USER_AGENT = config['REDDIT_USER_AGENT']
 
 reddit_instance = praw.Reddit(client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET, password=REDDIT_PASSWORD, 
-                              user_agent=REDDIT_USER_AGENT, username=REDDIT_USERNAME)
+                              user_agent=REDDIT_USER_AGENT, username=REDDIT_USERNAME, check_for_async=False)
 
 # Discord bot values
 DISCORD_TOKEN = config['DISCORD_TOKEN']
@@ -28,7 +56,7 @@ intents = discord.Intents.default()
 client = commands.Bot(command_prefix='!', intents=intents)
 
 # Subreddits that the bot will monitor
-sub_list = ['cats']
+sub_list = ['cats', 'AceAttorney']
 subreddits = {}
 submissions = {}
 last_submissions = []
@@ -39,6 +67,8 @@ for sub in sub_list:
 
 @client.event
 async def on_ready():
+    synced = await client.tree.sync()
+    print(f'Synced {len(synced)} command(s)')
     await sub_update.start()
 
 # Newest post loop
@@ -54,5 +84,26 @@ async def sub_update():
         submissions[f'{sub}'] = next(subreddits[f'{sub}'].new(limit=1))
         await channel.send(submissions[f'{sub}'].title)
 
+# Set channel command
+@client.tree.command(name='set_sub_to_channel', 
+                     description='Sets subreddit posts to be sent to this channel, multiple subreddits can be set separating by space.')
+async def sub_to_channel(interaction: discord.Interaction, subs: str):
+    subs_list = subs.split()
+    channel = interaction.channel
+    guild = interaction.guild
+
+    for sub in subs_list:
+        sub_query = session.query(exists().where(and_(Subreddit.name == sub, Subreddit.channel_id == channel.id))).scalar()
+
+        if sub_query:
+            continue
+
+        sub_set = Subreddit(guild.id, channel.id, sub)
+        session.add(sub_set)
+        session.commit()
+    
+    await interaction.response.send_message(f'{len(subs_list)} subreddit(s) set to this channel!')
+
 # Runs Discord Bot
-client.run(DISCORD_TOKEN)
+if __name__ == '__main__':
+    client.run(DISCORD_TOKEN)
